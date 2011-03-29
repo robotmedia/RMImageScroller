1f7040f67d365ab79053f13e7f3421e5e4a61d14
Hermes Pique <github@robotmedia.net>
2011-03-29 02:33:11 +0200

    first commit

--- Other/RMImageScrollerAppDelegate.m
+++ Other/RMImageScrollerAppDelegate.m
@@ -2,9 +2,23 @@
 //  RMImageScrollerAppDelegate.m
 //  RMImageScroller
 //
-//  Created by Pique on 3/28/11.
-//  Copyright 2011 Robot Media. All rights reserved.
+//  Created by Hermes Pique on 3/27/11.
+//	Copyright 2011 Robot Media SL <http://www.robotmedia.net>. All rights reserved.
 //
+//	This file is part of RMImageScroller.
+//
+//	RMImageScroller is free software: you can redistribute it and/or modify
+//	it under the terms of the GNU Lesser Public License as published by
+//	the Free Software Foundation, either version 3 of the License, or
+//	(at your option) any later version.
+//
+//	RMImageScroller is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU Lesser Public License for more details.
+//
+//	You should have received a copy of the GNU Lesser Public License
+//	along with RMImageScroller.  If not, see <http://www.gnu.org/licenses/>.
 
 #import "RMImageScrollerAppDelegate.h"
 #import "RMImageScrollerViewController.h"
